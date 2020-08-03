4dfb42162b0028ec989a7c9cf71c7f01eea3d550
zimo <461009747@qq.com>
2020-08-03 23:22:35 +0800

    更新springboot版本

---
+++ src/main/java/wiki/zimo/wiseduunifiedloginapi/controller/IndexController.java
@@ -0,0 +1,12 @@
+package wiki.zimo.wiseduunifiedloginapi.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+    @RequestMapping("/")
+    public String index() {
+        return "redirect:/swagger-ui.html";
+    }
+}
