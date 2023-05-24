48b0017aaafbef78717f753c6798de65e466fd17
sanseyooyea <1187586838@qq.com>
2023-05-24 23:37:44 +0800

    优化LoginService

--- src/main/java/wiki/zimo/wiseduunifiedloginapi/process/University.java
+++ src/main/java/wiki/zimo/wiseduunifiedloginapi/process/University.java
@@ -1,5 +1,11 @@
 package wiki.zimo.wiseduunifiedloginapi.process;
 
+import org.apache.commons.lang3.StringUtils;
+import wiki.zimo.wiseduunifiedloginapi.process.impl.*;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.Map;
+
 /**
  * @author SanseYooyea
  */
@@ -7,57 +13,87 @@ public enum University {
     /**
      * 安徽建筑大学
      */
-    AHJZU,
+    AHJZU("ahjzu.edu.cn", AhjzuCasLoginProcess.class),
     /**
      *
      */
-    BISTU,
+    BISTU("bistu.edu.cn", BistuLoginProcess.class),
     /**
      *
      */
-    CAS,
+    CAS("", CasLoginProcess.class),
     /**
      * 中国计量大学
      */
-    CJLU,
+    CJLU("cjlu.edu.cn", CjluLoginProcess.class),
     /**
      * 成都信息工程大学
      */
-    CUIT,
+    CUIT("cuit.edu.cn", CuitCasLoginProcess.class),
     /**
      * 中国矿业大学
      */
-    CUMT,
+    CUMT("cumt.edu.cn", CumtCasLoginProcess.class),
     /**
      * 河南大学
      */
-    HENU,
+    HENU("henu.edu.cn", HenuCasLoginProcess.class),
     /**
      * 合肥师范学院
      */
-    HFNU,
+    HFNU("hfnu.edu.cn", HfnuCasLoginProcess.class),
     /**
      * 合肥工业大学
      */
-    HFUT,
+    HFUT("hfut.edu.cn", HfutCasLoginProcess.class),
     /**
      * 淮阴师范学院
      */
-    HYTC,
+    HYTC("hytc.edu.cn", HytcCasLoginProcess.class),
     /**
      *
      */
-    IAP,
+    IAP("/iap", IapLoginProcess.class),
     /**
      * 江苏科技大学
      */
-    KMU,
+    KMU("kmu.edu.cn", KmuCasLoginProcess.class),
     /**
      * 昆明学院
      */
-    SDU,
+    SDUC("sduc.edu.cn", SducCasLoginProcess.class),
     /**
      * 武汉轻工大学
      */
-    WHPU;
+    WHPU("whpu.edu.cn", WhpuCasLoginProcess.class);
+
+    String flagUrl;
+    Class<?> processClazz;
+
+    University(String flagUrl, Class<?> processClazz) {
+        this.flagUrl = flagUrl;
+        this.processClazz = processClazz;
+    }
+
+    public static University getUniversityByLoginUrl(String loginUrl) {
+        for (University university : University.values()) {
+            if (StringUtils.isEmpty(university.flagUrl)) {
+                continue;
+            }
+
+            if (loginUrl.trim().contains(university.flagUrl)) {
+                return university;
+            }
+        }
+        return CAS;
+    }
+
+    public BaseLoginProcess newLoginProcess(String loginUrl, Map<String, String> parms) {
+        try {
+            return (BaseLoginProcess) processClazz.getDeclaredConstructor(String.class, Map.class).newInstance(loginUrl, parms);
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException |
+                 NoSuchMethodException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
--- src/main/java/wiki/zimo/wiseduunifiedloginapi/service/impl/LoginServiceImpl.java
+++ src/main/java/wiki/zimo/wiseduunifiedloginapi/service/impl/LoginServiceImpl.java
@@ -3,7 +3,8 @@ package wiki.zimo.wiseduunifiedloginapi.service.impl;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import wiki.zimo.wiseduunifiedloginapi.process.impl.*;
+import wiki.zimo.wiseduunifiedloginapi.process.BaseLoginProcess;
+import wiki.zimo.wiseduunifiedloginapi.process.University;
 import wiki.zimo.wiseduunifiedloginapi.service.LoginService;
 
 import java.util.HashMap;
@@ -31,52 +32,9 @@ public class LoginServiceImpl implements LoginService {
         params.put("password", password);
 
         // 根据login_url判断类型
-        if (login_url.trim().contains("/iap")) {
-            IapLoginProcess process = new IapLoginProcess(login_url, params);
-            return process.login();
-        } else if (login_url.trim().contains("cumt.edu.cn")) {
-            CumtCasLoginProcess process = new CumtCasLoginProcess(login_url, params);
-            return process.login();
-        } else if (login_url.trim().contains("henu.edu.cn")) {
-            HenuCasLoginProcess process = new HenuCasLoginProcess(login_url, params);
-            return process.login();
-        } else if (login_url.trim().contains("ahjzu.edu.cn")) {
-            AhjzuCasLoginProcess process = new AhjzuCasLoginProcess(login_url, params);
-            return process.login();
-        } else if (login_url.trim().contains("kmu.edu.cn")) {
-            KmuCasLoginProcess process = new KmuCasLoginProcess(login_url, params);
-            return process.login();
-        } else if (login_url.trim().contains("whpu.edu.cn")) {
-            WhpuCasLoginProcess process = new WhpuCasLoginProcess(login_url, params);
-            return process.login();
-        } else if (login_url.trim().contains("sduc.edu.cn")) {
-            SducCasLoginProcess process = new SducCasLoginProcess(login_url, params);
-            return process.login();
-        } else if (login_url.trim().contains("cuit.edu.cn")) {
-            CuitCasLoginProcess process = new CuitCasLoginProcess(login_url, params);
-            return process.login();
-        } else if (login_url.trim().contains("hfnu.edu.cn")) {
-            HfnuCasLoginProcess process = new HfnuCasLoginProcess(login_url, params);
-            return process.login();
-        } else if (login_url.trim().contains("hfut.edu.cn")) {
-            HfutCasLoginProcess process = new HfutCasLoginProcess(login_url, params);
-            return process.login();
-        } else if (login_url.trim().contains("just.edu.cn")) {
-            JustCasLoginProcess process = new JustCasLoginProcess(login_url, params);
-            return process.login();
-        } else if (login_url.trim().contains("hytc.edu.cn")) {
-            HytcCasLoginProcess process = new HytcCasLoginProcess(login_url, params);
-            return process.login();
-        } else if (login_url.trim().contains("bistu.edu.cn")) {
-            BistuLoginProcess process = new BistuLoginProcess(login_url, params);
-            return process.login();
-        } else if (login_url.trim().contains("cjlu.edu.cn")) {
-            CjluLoginProcess process = new CjluLoginProcess(login_url, params);
-            return process.login();
-        } else {
-            CasLoginProcess process = new CasLoginProcess(login_url, params);
-            return process.login();
-        }
+        University university = University.getUniversityByLoginUrl(login_url);
+        BaseLoginProcess process = university.newLoginProcess(login_url, params);
+        return process.login();
     }
 
 
